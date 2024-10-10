f652a131221f16dcca3983d01eb096993b45dc26
solo-source <samrendrapersonal@gmail.com>
2024-10-11 00:22:57 +0530

    Solved the final question of day one.

---
+++ 100DaysOfCode/day-one/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char inputCharacter;
+    bool output = false;
+
+    cout << "Enter the input character: " << endl;
+    cin >> inputCharacter;
+
+    if((inputCharacter == 'A' || inputCharacter == 'A' + 32) || (inputCharacter == 'E' || inputCharacter == 'E' + 32)
+         || (inputCharacter == 'I' || inputCharacter == 'I' + 32) || (inputCharacter == 'O' || inputCharacter == 'O' + 32)
+         || (inputCharacter == 'U' || inputCharacter == 'U' + 32))
+         {
+            cout << "Vowel" << endl;
+         }
+    else if (inputCharacter >= 65 && inputCharacter <= 122)
+    {
+        cout << "Consonant" << endl;
+    }
+    else if (inputCharacter >= 48 && inputCharacter <= 57)
+    {
+        cout << "Invalid Input" << endl;
+    }
+    
+}
